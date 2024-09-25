07c616ea0f0391850a5c2e2cc96baf5fb5a6cdaf
Lucas Alves <lcalves012@gmail.com>
2024-09-24 22:08:47 -0300

    feat(1): Criando estrutura do projeto.

---
+++ src/main/java/com/AppCores/AppCores/AppCoresApplication.java
@@ -0,0 +1,13 @@
+package com.AppCores.AppCores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AppCores/AppCores/AppCoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppCores.AppCores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
